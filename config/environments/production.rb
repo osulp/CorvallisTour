342a679cc3eec294b25752ea3bf7612376df0399
Trey Terrell <trey.terrell@oregonstate.edu>
2014-01-29 17:17:12 -0800

    Add home.css.scss to precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ CorvallisTour::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( home.css.scss )
+  config.assets.precompile += %w( home.css.scss home.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
